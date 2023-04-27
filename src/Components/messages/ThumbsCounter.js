bb63122ca40817024bb1fed41be9ce35472b54d3
Kiersten White <kierstenswhite@gmail.com>
2023-04-27 13:33:14 -0400

    little edits

--- src/Components/messages/ThumbsCounter.js
+++ src/Components/messages/ThumbsCounter.js
@@ -4,7 +4,7 @@ import { GetIndividualMessages, GetMessageAndReplace } from "./MessageAPIManager
 
 export const ThumbsCounter = () => {
 
-    const [message, editMe]
+    // const [message, editMe]
     let [likes, setLikes] = useState(0)
     let [dislikes, setDislikes] = useState(0)
 
@@ -29,56 +29,56 @@ export const ThumbsCounter = () => {
 
 
 
-export const MessageEdit = () => {
+// export const MessageEdit = () => {
 
-    const [message, editMessage] = useState({
-        message: "",
-        username: "",
-        messageId: 0
-    })
+//     const [message, editMessage] = useState({
+//         message: "",
+//         username: "",
+//         messageId: 0
+//     })
 
-    const { messageId } = useParams()
-    const navigate = useNavigate()
+//     const { messageId } = useParams()
+//     const navigate = useNavigate()
 
-    useEffect(() => {
-        GetIndividualMessages(messageId)
-            .then((data) => {
-                editMessage(data)
-            })
-    }, [ messageId ])
+//     useEffect(() => {
+//         GetIndividualMessages(messageId)
+//             .then((data) => {
+//                 editMessage(data)
+//             })
+//     }, [ messageId ])
 
-    const handleSaveButtonClick = (event) => {
-        event.preventDefault()
+//     const handleSaveButtonClick = (event) => {
+//         event.preventDefault()
 
-        GetMessageAndReplace(message)
-        .then(() => {
-            navigate("/")
-        })
-    }
+//         GetMessageAndReplace(message)
+//         .then(() => {
+//             navigate("/")
+//         })
+//     }
 
-    return <form className="messageEditForm">
-        <h2 className="messageEdit_title">Edit Your Message</h2>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="message">Message:</label>
-                    <textarea
-                        required autoFocus
-                        type="text"
-                        className="form-control"
-                        value={message.message}
-                        onChange={
-                           (evt) => {
-                            const copy = {...message}
-                            copy.message = evt.target.value
-                            editMessage(copy)
-                           } 
-                        }>{message.message}</textarea>
-                </div>
-            </fieldset>
-            <button
-                onClick={(clickEvent) => handleSaveButtonClick(clickEvent)}
-                className="btn btn-primary">
-                    Update Message
-                </button>
-    </form>
-}
+//     return <form className="messageEditForm">
+//         <h2 className="messageEdit_title">Edit Your Message</h2>
+//             <fieldset>
+//                 <div className="form-group">
+//                     <label htmlFor="message">Message:</label>
+//                     <textarea
+//                         required autoFocus
+//                         type="text"
+//                         className="form-control"
+//                         value={message.message}
+//                         onChange={
+//                            (evt) => {
+//                             const copy = {...message}
+//                             copy.message = evt.target.value
+//                             editMessage(copy)
+//                            } 
+//                         }>{message.message}</textarea>
+//                 </div>
+//             </fieldset>
+//             <button
+//                 onClick={(clickEvent) => handleSaveButtonClick(clickEvent)}
+//                 className="btn btn-primary">
+//                     Update Message
+//                 </button>
+//     </form>
+// }
